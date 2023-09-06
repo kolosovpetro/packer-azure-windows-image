dc6bb1b5b8f81982d7de67ad338e145d44f64e11
kolosovpetro <kolosovp94@gmail.com>
2023-09-06 18:30:53 +0200

    agent install script

---
+++ scripts/install_agent.ps1
@@ -0,0 +1,71 @@
+param(
+    [String]$pat,
+    [String]$organizationUrl = "https://dev.azure.com/PetroKolosovProjects/",
+    [String]$environmentName = "dev",
+    [String]$projectName = "packer-azure-windows-image"
+)
+
+$ErrorActionPreference = "Stop";
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+{
+    throw "Run command in an administrator PowerShell prompt"
+};
+
+If ($PSVersionTable.PSVersion -lt (New-Object System.Version("3.0")))
+{
+    throw "The minimum version of Windows PowerShell that is required by the script (3.0) does not match the currently running version of Windows PowerShell."
+};
+
+If (-NOT(Test-Path $env:SystemDrive\'azagent'))
+{
+    mkdir $env:SystemDrive\'azagent'
+};
+
+cd $env:SystemDrive\'azagent';
+
+for($i = 1; $i -lt 100; $i++)
+{
+    $destFolder = "A" + $i.ToString();
+
+    if (-NOT(Test-Path ($destFolder)))
+    {
+        mkdir $destFolder;
+        cd $destFolder;
+        break;
+    }
+};
+
+$agentZip = "$PWD\agent.zip";
+
+$DefaultProxy = [System.Net.WebRequest]::DefaultWebProxy;
+
+$securityProtocol = @();
+
+$securityProtocol += [Net.ServicePointManager]::SecurityProtocol;
+
+$securityProtocol += [Net.SecurityProtocolType]::Tls12;
+
+[Net.ServicePointManager]::SecurityProtocol = $securityProtocol;
+
+$WebClient = New-Object Net.WebClient;
+
+$Uri = 'https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-win-x64-3.225.0.zip';
+
+if ($DefaultProxy -and (-not $DefaultProxy.IsBypassed($Uri)))
+{
+    $WebClient.Proxy = New-Object Net.WebProxy($DefaultProxy.GetProxy($Uri).OriginalString, $True);
+};
+
+$WebClient.DownloadFile($Uri, $agentZip);
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem;
+
+[System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, "$PWD");
+
+.\config.cmd --environment --environmentname $environmentName --agent $env:COMPUTERNAME `
+    --runasservice --work '_work' --url $organizationUrl `
+    --projectname $projectName `
+    --auth PAT --token $pat;
+
+Remove-Item $agentZip;
